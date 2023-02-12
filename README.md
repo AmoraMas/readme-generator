878eee4da04cdc854a40333a8124b0e7be29106f
AmoraMas <amoramas@protonmail.com>
2023-02-12 09:55:59 -0700

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# readme-generator
-Application that generates a standardized readme to add to GitHub projects.
+# GitHub Readme Generator
+
+> Web form that generates a Readme for your project after asking questions about it.
+
+## Table of Contents
+  - [General Information](#general-information)
+    - [Technologies Used](#technologies-used)
+    - [Features](#features)
+    - [Screenshots](#screenshots)
+    - [Setup](#setup)
+    - [Usage](#usage)
+    - [Current Status](#current-status)
+  - [Room For Improvement](#room-for-improvement)
+  - [Acknowledgements](#acknowledgements)
+  - [Contact](#contact)
+  - [License](#license)
+
+
+## General Information
+  > 
+
+## Technologies Used
+  > CSS3, HTML5, Javascript, jQuery 3.6.1
+
+## Features
+  - Web form saves data across uses, but can be easily reset to defautl values.
+  - Can save your default entries in the .js file under the defaultValues object.
+  - Helps provide a consistent readme appearance across all your projects.
+  - 
+  - 
+
+## Screenshot(s)
+  > ![ScreenShot](./images/Screenshot_1.png)
+
+## Setup
+  - None needed. Click on demonstration link below.
+- Or to run locally, copy all three index.xxxx files into a directory and point your browser to the index.html page.
+  
+[Project Demonstration](https://AmoraMas.github.io/RepoName/)
+
+## Usage
+  - Fill in all the form fields with correct information.
+- Click the Submit button.
+- Copy/Paste the results into your projects README.md file.
+- Make adjustments as needed.
+
+## Current Status
+  > Working, but needs improvement.
+
+## Room For Improvement
+  - Ideas:
+    - Adjust ToC and output to not include empty form fields.
+    - Adjust scroll so that it doesn't cover up title/button section.
+    - Adjust some fields to be combo/dropdown boxes for selectable items.
+  - Todo:
+    - Save info to README.md file.
+    - Add license section.
+    - Add tests to ensure required fields are present.
+
+## Acknowledgements
+  - ### Inspired By: https://github.com/kevin-foreman/README-Generator
+  - ### Based On: https://bulldogjob.com/readme/how-to-write-a-good-readme-for-your-github-project
+  - ### Contributors: 
+
+## Contact
+  > [amoramas1984@gmail.com](mailto:amoramas1984@gmail.com)
+
+## License
+  > 
---
+++ index.js
@@ -0,0 +1,113 @@
+
+
+// DEFAULT ANSWERS  (the key should be the id of each input/textarea field)
+const defaultValues = {
+  screenshot: '/images/screenshot.png',
+  contributors: 'None',
+  email: 'amoramas1984@gmail.com',
+  technologies: 'CSS3, HTML5, JavaScript',
+  setup: 'None Needed. Click on demonstration link below.',
+  demo: 'https://AmoraMas.github.io/RepoName/',
+  license: ''
+}
+
+let $submitButton = $('.submitButton');
+$submitButton.click(function () {
+  createReadme();
+});
+
+let $resetButton = $('.resetButton');
+$resetButton.click(resetPage);
+
+function resetPage() {
+  let $allInputs = $('input[type=text], textarea');
+  for (let i = 0; i < $allInputs.length; i++) {
+    let id = $($allInputs[i]).attr('id')
+    if (id in defaultValues) {
+      $($allInputs[i]).val(defaultValues[id]);
+    }
+    else {
+      $($allInputs[i]).val('');
+    }
+  }
+}
+
+function createTOC() {
+  let content = `## Table of Contents
+  - [General Information](#general-information)
+    - [Technologies Used](#technologies-used)
+    - [Features](#features)
+    - [Screenshots](#screenshots)
+    - [Setup](#setup)
+    - [Usage](#usage)
+    - [Current Status](#current-status)
+  - [Room For Improvement](#room-for-improvement)
+  - [Acknowledgements](#acknowledgements)
+  - [Contact](#contact)
+  - [License](#license)
+`
+  return content;
+}
+
+// Function that generates the Readme text based on the information the user provided.
+function createReadme() {
+  let content = `# ${$("#name").val()}
+
+> ${$('#description').val()}
+
+${createTOC()}
+
+## General Information
+  > ${$('#info').val()}
+
+## Technologies Used
+  > ${$('#technologies').val()}
+
+## Features
+  - ${$('input[name="project_features_1"]').val()}
+  - ${$('input[name="project_features_2"]').val()}
+  - ${$('input[name="project_features_3"]').val()}
+  - ${$('input[name="project_features_4"]').val()}
+  - ${$('input[name="project_features_5"]').val()}
+
+## Screenshot(s)
+  > ![ScreenShot](${$('#screenshot').val()})
+
+## Setup
+  ${$('#setup').val()}
+  
+[Project Demonstration](${$('#demo').val()})
+
+## Usage
+  ${$('#usage').val()}
+
+## Current Status
+  > ${$('#status').val()}
+
+## Room For Improvement
+  - Ideas:
+    - ${$('input[name="project_improvements_1"]').val()}
+    - ${$('input[name="project_improvements_2"]').val()}
+    - ${$('input[name="project_improvements_3"]').val()}
+  - Todo:
+    - ${$('input[name="project_todo_1"]').val()}
+    - ${$('input[name="project_todo_2"]').val()}
+    - ${$('input[name="project_todo_3"]').val()}
+
+## Acknowledgements
+  - ### Inspired By: ${$('#inspiredBy').val()}
+  - ### Based On: ${$('#basedOn').val()}
+  - ### Contributors: ${$('#contributors').val()}
+
+## Contact
+  > [${$('#email').val()}](mailto:${$('#email').val()})
+
+## License
+  > ${$('#license').val()}
+`
+  let $contentBlock = $('<pre></pre>').addClass('contentBlock');
+  $contentBlock.append(content);
+  $('.readmeSection').empty();
+  $('.readmeSection').append($contentBlock);
+}
+
